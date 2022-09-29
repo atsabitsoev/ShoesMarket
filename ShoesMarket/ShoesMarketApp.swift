b58e3475319ee7f0a155e9370088bfaabff41b21
Ацамаз Бицоев <bitsoevAS@msk.bcs.ru>
2022-09-29 19:52:44 +0300

    Initial Commit

---
+++ ShoesMarket/ShoesMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoesMarketApp.swift
+//  ShoesMarket
+//
+//  Created by Ацамаз Бицоев on 29.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoesMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
